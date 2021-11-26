3506c45eda4ba418d3c833743f61113cbb642e4a
Sana <sanabarilad@gmail.com>
2021-11-26 13:05:17 +0100

    changed date picker variables, maxDate, minDate

--- frontend/datepicker.js
+++ frontend/datepicker.js
@@ -1,5 +1,12 @@
+
+//Date picker Jquery
 $(document).ready(function () {
-  $("#datepicker").datepicker();
+  $("#datepicker").datepicker({
+    //maximum 30days 
+    maxDate: "+1m",
+    //Start date is always today, new Date() is current date object
+    minDate: new Date(),
+  });
 });
 
 $(function () {
