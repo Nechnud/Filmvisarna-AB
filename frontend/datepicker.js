9d08130cb91bd3d88ec1030abce3f7b1b93d0869
Sana <sanabarilad@gmail.com>
2021-11-26 18:02:37 +0100

    added javascript code for film screening

--- frontend/datepicker.js
+++ frontend/datepicker.js
@@ -10,9 +10,70 @@ $(document).ready(function () {
   });
 });
 
+let showJson;
+let filmNumber;
+
+
+async function readShowJson() {
+  showJson = await $.getJSON('/json/shows.json');
+}
+
+const filmsSelect = document.querySelector('#filmsToShow');
+filmsSelect.addEventListener('change', (event) => {
+  let html = '';
+  filmNumber = event.target.value;
+  readShowJson();
+  if (event.target.value === 'All Films') {
+    for (i = 0; i < showJson.length; i++) {
+      html += `
+      <tr>
+      <th>${showJson[i].title}</th>
+      </tr>
+    `;
+    }
+  }
+  if (event.target.value != 'All Films') {
+
+    html += `
+      <tr class="table-result">
+      <th>${event.target.value}</th>   
+      </tr>
+      `;
+  }
+  //console.log(event.target.value);
+  $('.screening-result').html(html);
+  return filmNumber;
+});
+
+
+//These codes are not ready yet! ------------------------- Have a nice weekend!
+let date, dateToShow;
 $(function () {
-  $("#datepicker").on("change", function () {
-    let date = $(this).val();
-    alert(date);
+  $("#datepicker").on('change', function () {
+    date = $(this).val();
+    console.log(date);
+    if (filmNumber === 'All Films') {
+      for (i = 0; i < showJson.length; i++) {
+        dateToShow = `
+          <td>${date}</td>   
+          `;
+      }
+    }
+    if (filmNumber != 'All Films') {
+      dateToShow += `
+      <td>${date}</td>   
+      `;
+    }
+    $('.table-result').html(date);
   });
 });
+
+
+const salongSelect = document.querySelector('#salong');
+salongSelect.addEventListener('change', (event) => {
+  let date = '';
+  if (filmNumber === 'All Films') {
+
+  }
+});
+//These codes are not ready yet! -------------------------
