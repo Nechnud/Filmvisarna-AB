23cbc7d2450f529c06b699c6cb39c3daee5b92df
Sana <sanabarilad@gmail.com>
2021-11-26 10:52:26 +0100

    changed movie-carousel

--- frontend/main.js
+++ frontend/main.js
@@ -6,19 +6,43 @@ $('.datepicker').datepicker({
   todayHighlight: true
 });
 
+//Codes for showing the movie information when the user click on the movie poster ---------
 let films;
-async function readFilmInformation() {
-  films = await $.getJSON('showing.json');
-}
+let movieID;
 
+$('.movie').click(function () {
+  movieID = $(this).attr('id');
+  readFilmsFromJson(movieID);
+
+  $(this).find('.movieInfo').toggle();
+});
+
+async function readFilmsFromJson(movieID) {
+  let html = '';
+  films = await $.getJSON('json/movieinfo.json');
+  for (i = 0; i < films.length; i++) {
+    if (films[i].id == movieID) {
+
+      html += `
+      <div class="movieInfo">
+      <h3>${films[i].title}</h3>
+      <p><b>Year: </b>${films[i].year}</p>
+      <p><b>Runtime: </b>${films[i].runtime}</p>
+          `;
+    }
+  }
+  $('.' + movieID).html(html);
+}
+readFilmsFromJson();
+//-------------------------------------------------------------
 
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes()
 var dateTime = date + ' ' + time;
 
 
-$(datepicker).click(function (e) { 
+$(datepicker).click(function (e) {
   e.preventDefault();
-  
+
 });
