8f389a89f08c40e3ff13b74b19be99fa80ab1f0a
herrgallardo <gallardogirela@gmail.com>
2021-11-29 14:02:11 +0100

    added javascript to filminfo.js

---
+++ frontend/filminfo.js
@@ -0,0 +1,37 @@
+let movieId = 1;
+
+if (movieId === 1) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
+
+if (movieId === 2) {
+  document.getElementById("movie-title").innerText = "Joker";
+  document.getElementById("movie-image").src = "/images/joker.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
+
+if (movieId === 3) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
+
+if (movieId === 4) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
+
+if (movieId === 5) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
+
+if (movieId === 6) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
+}
