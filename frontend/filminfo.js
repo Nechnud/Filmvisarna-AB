679f0a6e49543fa039812e23b9bc3cf8e4c84fbd
herrgallardo <gallardogirela@gmail.com>
2021-11-29 14:58:39 +0100

    changed filminfo.js

--- frontend/filminfo.js
+++ frontend/filminfo.js
@@ -1,38 +1,39 @@
+let movieId = 6;
 
-function readTheID(movieID) {
-  if (movieID == 1) {
-    document.getElementById("movie-title").innerText = "The Day After Tomorrow";
-    document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+let movieInfo;
 
-  if (movieID === 2) {
-    document.getElementById("movie-title").innerText = "Joker";
-    document.getElementById("movie-image").src = "/images/joker.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+if (movieId === 1) {
+  document.getElementById("movie-title").innerText = "The Day After Tomorrow";
+  document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/Ku_IseK3xTc";
+}
 
-  if (movieID === 3) {
-    document.getElementById("movie-title").innerText = "The Day After Tomorrow";
-    document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+if (movieId === 2) {
+  document.getElementById("movie-title").innerText = "Joker";
+  document.getElementById("movie-image").src = "/images/joker.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/zAGVQLHvwOY";
+}
 
-  if (movieID === 4) {
-    document.getElementById("movie-title").innerText = "The Day After Tomorrow";
-    document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+if (movieId === 3) {
+  document.getElementById("movie-title").innerText = "Aladdin";
+  document.getElementById("movie-image").src = "/images/aladdin.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/eTjHiQKJUDY";
+}
 
-  if (movieID === 5) {
-    document.getElementById("movie-title").innerText = "The Day After Tomorrow";
-    document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+if (movieId === 4) {
+  document.getElementById("movie-title").innerText = "My Neighbour Totoro";
+  document.getElementById("movie-image").src = "/images/totoro.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/92a7Hj0ijLs";
+}
 
-  if (movieID === 6) {
-    document.getElementById("movie-title").innerText = "The Day After Tomorrow";
-    document.getElementById("movie-image").src = "/images/dayAfterTomorrow.jpg";
-    document.getElementById("movie-link").src = "https://www.youtube.com/embed/INxRqDnY_BY";
-  }
+if (movieId === 5) {
+  document.getElementById("movie-title").innerText = "Interstellar";
+  document.getElementById("movie-image").src = "/images/interstellar.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/zSWdZVtXT7E";
+}
+
+if (movieId === 6) {
+  document.getElementById("movie-title").innerText = "Spider-Man: No way home";
+  document.getElementById("movie-image").src = "/images/spiderman.jpg";
+  document.getElementById("movie-link").src = "https://www.youtube.com/embed/JfVOs4VSpmA";
 }
