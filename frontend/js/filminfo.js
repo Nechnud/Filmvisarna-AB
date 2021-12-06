faaaf06b46a80adc4d0998e9261029fa6c512089
herrgallardo <gallardogirela@gmail.com>
2021-12-06 15:57:20 +0100

    changes to filminfo.js

--- frontend/js/filminfo.js
+++ frontend/js/filminfo.js
@@ -1,6 +1,3 @@
-
-
-
 let movieId = localStorage.getItem("ID");
 
 let movies;
@@ -10,7 +7,6 @@ let movieData;
 async function readJson() {
   let rawData = await fetch('json/movieinfo.json');
   movies = await rawData.json();
-  console.log(movies);
   getMovieToShow(movies);
 }
 
@@ -18,15 +14,12 @@ readJson();
 
 function getMovieToShow() {
   for (let i = 0; i < movies.length; i++) {
-    if (movies[i].id === movieId) {
+    if (movies[i].id == movieId) {
       movieToShow = movies[i];
     }
   }
   renderMovieInfo(movieToShow);
 }
-
-
-
 
 function renderMovieInfo() {
   movieData =
@@ -40,45 +33,3 @@ function renderMovieInfo() {
   document.getElementById("movie-image").src = movieToShow.image_portrait_m;
   document.getElementById("movie-trailer").src = movieToShow.trailer;
 }
-
-
-
-
-
-
-/*if (movieId === 1) {
-  document.getElementById("movie-info").innerText = renderMovieInfo();
-  document.getElementById("movie-image").src = movieToShow.image_portrait_m;
-  document.getElementById("movie-trailer").src = movieToShow.trailer;
-}
-
-if (movieId === 2) {
-  document.getElementById("movie-info").innerText = "Joker";
-  document.getElementById("movie-image").src = "images/jokerM.jpg";
-  document.getElementById("movie-trailer").src = "https://www.youtube.com/embed/zAGVQLHvwOY";
-}
-
-if (movieId === 3) {
-  document.getElementById("movie-info").innerText = "Aladdin";
-  document.getElementById("movie-image").src = "images/aladdinM.jpg";
-  document.getElementById("movie-trailer").src = "https://www.youtube.com/embed/eTjHiQKJUDY";
-}
-
-if (movieId === 4) {
-  document.getElementById("movie-info").innerText = "My Neighbour Totoro";
-  document.getElementById("movie-image").src = "images/totoroM.jpg";
-  document.getElementById("movie-trailer").src = "https://www.youtube.com/embed/92a7Hj0ijLs";
-}
-
-if (movieId === 5) {
-  document.getElementById("movie-info").innerText = "Interstellar";
-  document.getElementById("movie-image").src = "images/interstellarM.jpg";
-  document.getElementById("movie-trailer").src = "https://www.youtube.com/embed/zSWdZVtXT7E";
-}
-
-if (movieId === 6) {
-  document.getElementById("movie-info").innerText = "Spider-Man: No way home";
-  document.getElementById("movie-image").src = "images/spidermanM.jpg";
-  document.getElementById("movie-trailer").src = "https://www.youtube.com/embed/JfVOs4VSpmA";
-}
-*/
