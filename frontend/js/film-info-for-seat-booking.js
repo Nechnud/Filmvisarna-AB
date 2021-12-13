15da0ae8ea9dce10b066cc3921d139be50618d70
herrgallardo <gallardogirela@gmail.com>
2021-12-13 12:21:59 +0100

    created js file to show filminfo on booking screen

---
+++ frontend/js/film-info-for-seat-booking.js
@@ -0,0 +1,29 @@
+let movieImage;
+async function readJson() {
+  let rawData = await fetch('json/movieinfo.json');
+  movies = await rawData.json();
+  renderMovieInfo(movies);
+}
+
+readJson();
+
+function renderMovieInfo() {
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].title == localStorage.getItem('movieTitle')) {
+      movieImage = movies[i].image_portrait_s;
+    }
+  }
+  movieData =
+    `
+    <p>${localStorage.getItem('movieTitle')}</p>
+    <p>Date: ${localStorage.getItem('date')}</p>
+    <p>Salon: ${localStorage.getItem('salong')}</p>
+    <img src="${movieImage}">
+    
+           
+  `;
+  document.getElementById("film-info-booking").innerHTML = movieData;
+  console.log(movieData);
+
+}
+
