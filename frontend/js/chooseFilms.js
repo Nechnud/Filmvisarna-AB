ca39359ba6636caff9ee20c5d6bc88ad93bccde9
Sana <sanabarilad@gmail.com>
2021-12-14 11:59:36 +0100

    changed hard coded part to JS code chooseFilms.js

--- frontend/js/chooseFilms.js
+++ frontend/js/chooseFilms.js
@@ -14,15 +14,23 @@ async function showAllMovies() {
   trailerSRC = '';
 
   for (i = 0; i < readJson2.length; i++) {
-    html = `
-    
+    html += `
+    <div class="row">
+            <div class="col">
+              <a href="filminfo.html"><img src="${readJson2[i].image_portrait_s}" id="${i + 1}" class="movieAll"></a>
+            </div>
+            <div class="col">
+              <div id="screening-result${readJson2[i].id}" class="col "></div>
+           
     <p class="name">${readJson2[i].title}</p>
       <button id="${readJson2[i].title}" type="button" class="trailer-button" data-bs-toggle="modal"
               data-bs-target="#exampleModal">
               PLAY TRAILER
             </button>
+            </div>
+          </div>
     `;
-    $('#screening-result' + `${readJson2[i].id}`).html(html);
+    $('#screening-result').html(html);
   }
 
   $('.trailer-button').on('click', function () {
@@ -34,16 +42,18 @@ async function showAllMovies() {
     }
     $('#movieTrailer').attr('src', trailerSRC);
   });
+  getMovieID();
 }
 
-$('.movieAll').on('click', function () {
-  //Get the id of the clicked html tag
-  movieID = $(this).attr('id');
-  //Store the movie id to localStorage so that when we change the webpage 
-  //we could still get the movie id from localStorage
-  localStorage.setItem('ID', movieID);
+function getMovieID() {
+  $('.movieAll').on('click', function () {
+    //Get the id of the clicked html tag
+    movieID = $(this).attr('id');
+    //Store the movie id to localStorage so that when we change the webpage 
+    //we could still get the movie id from localStorage
+    localStorage.setItem('ID', movieID);
 
-});
-
+  });
+}
 readInfoJson();
 
