d4235fcd87b2871042e9602c9547e9443fbd6740
Sana <sanabarilad@gmail.com>
2021-12-16 14:18:31 +0100

    added age group filter button and JS code for it

--- frontend/js/chooseFilms.js
+++ frontend/js/chooseFilms.js
@@ -1,6 +1,7 @@
-let jsonMovies, jsonShows;
+let jsonMovies, jsonShows, movie, age;
 let trailerSRC, trialerID;
 let today = new Date();
+
 let date = today.getDate().toString() + "-" + (today.getMonth() + 1).toString() + "-" + today.getFullYear().toString();
 $(function () {
   $("#datepicker").datepicker({
@@ -17,12 +18,8 @@ async function readInfoJson() {
   jsonShows = await $.getJSON('json/shows.json');
   showAllMovies(jsonMovies);
 }
-
-async function showAllMovies() {
-  html = '';
-  trailerSRC = '';
-  for (i = 0; i < jsonMovies.length; i++) {
-    html += `
+function readHTML() {
+  html += `
     <div class="row">
             <div class="col">
               <a href="filminfo.html"><img src="${jsonMovies[i].image_portrait_s}" id="${i + 1}" class="movieAll"></a>
@@ -34,6 +31,7 @@ async function showAllMovies() {
     <p class="name">Salon: ${jsonShows[i].showRoom}</p>
     <p class="name">Time: ${jsonShows[i].showTime}</p>
     <p class="name">${date}</p>
+    <p class="name">Rated: ${jsonMovies[i].rated}</p>
       <button id="${jsonMovies[i].id}" type="button" class="trailer-button" data-bs-toggle="modal"
               data-bs-target="#trailerModal">
               PLAY TRAILER
@@ -41,9 +39,73 @@ async function showAllMovies() {
             </div>
           </div>
     `;
-    $('#screening-result').html(html);
+}
+function checkOptions() {
+  if (age == 'allAge' || age == 'children17' && movie == 0) {
+    showAllMovies();
   }
+  if (age == 'allAge' || age == 'children17' && movie != 0) {
+    filterMovies();
+  }
+  if (age == 'children' && movie == 0) {
+    ageLimit.push('G');
+    filterAges(ageLimit);
+  }
+  if (age == 'children13' && movie == 0) {
+    ageLimit.push('G');
+    ageLimit.push('PG-13');
+    filterAges(ageLimit);
+  }
+  if (age == 'children17' && movie == 0) {
+    ageLimit.push('G');
+    ageLimit.push('PG-13');
+    ageLimit.push('R');
+    filterAges(ageLimit);
+  }
+  if (age == 'children17' && movie != 0) {
+    ageLimit.push('G');
+    ageLimit.push('PG-13');
+    ageLimit.push('R');
+    ageAndMovies(ageLimit);
+  }
+  if (age == 'children' && movie != 0) {
+    ageLimit.push('G');
+    ageAndMovies(ageLimit);
+  }
+  if (age == 'children13' && movie != 0) {
+    ageLimit.push('G');
+    ageLimit.push('PG-13');
+    ageAndMovies(ageLimit);
+  }
+}
+
+const moviesSelect = document.querySelector('#chooseMovies');
+moviesSelect.addEventListener('change', (event) => {
+  movie = $("#chooseMovies :selected").val();
+  age = $("#chooseAge :selected").val();
+  checkOptions();
   checkTrialer();
+});
+
+$(function () {
+  $("#datepicker").on('change', function () {
+    date = $(this).val();
+    movie = $("#chooseMovies :selected").val();
+    age = $("#chooseAge :selected").val();
+    checkOptions();
+  });
+  checkTrialer();
+});
+
+function getMovieID() {
+  $('.movieAll').on('click', function () {
+    //Get the id of the clicked html tag
+    movieID = $(this).attr('id');
+    console.log(movieID);
+    //Store the movie id to localStorage so that when we change the webpage 
+    //we could still get the movie id from localStorage
+    localStorage.setItem('ID', movieID);
+  });
 }
 function checkTrialer() {
   $('.trailer-button').on('click', function () {
@@ -57,69 +119,70 @@ function checkTrialer() {
   });
 }
 
-let movie;
-const moviesSelect = document.querySelector('#chooseMovies');
-moviesSelect.addEventListener('change', (event) => {
+let ageLimit = [];
+const ageSelect = document.querySelector('#chooseAge');
+ageSelect.addEventListener('change', (event) => {
+  age = $("#chooseAge :selected").val();
   movie = $("#chooseMovies :selected").val();
-  if (movie == 0) {
-    showAllMovies();
-  }
-  else {
-    switchMovie(movie);
-  }
+  checkOptions();
   checkTrialer();
 });
-
-
-$(function () {
-  $("#datepicker").on('change', function () {
-    date = $(this).val();
-    movie = $("#chooseMovies :selected").val();
-    if (movie == 0) {
-      showAllMovies();
-    }
-    else {
-      switchMovie(movie);
-    }
-  });
-});
-
-async function switchMovie() {
+function showAllMovies() {
+  html = '';
+  trailerSRC = '';
   for (i = 0; i < jsonMovies.length; i++) {
-    if (movie == jsonMovies[i].id) {
-      html = `
-    <div class="row">
-            <div class="col">
-              <a href="filminfo.html"><img src="${jsonMovies[i].image_portrait_s}" id="${i + 1}" class="movieAll"></a>
-            </div>
-            <div class="col">
-              <div id="screening-result${jsonMovies[i].id}" class="col "></div>
-           
-    <p class="name">${jsonMovies[i].title}</p>
-    <p class="name">Salon: ${jsonShows[i].showRoom}</p>
-    <p class="name">Time: ${jsonShows[i].showTime}</p>
-    <p class="name">${date}</p>
-      <button id="${jsonMovies[i].id}" type="button" class="trailer-button" data-bs-toggle="modal"
-              data-bs-target="#trailerModal">
-              PLAY TRAILER
-            </button>
-            </div>
-          </div>
-    `;
-      $('#screening-result').html(html);
+    readHTML();
+    $('#screening-result').html(html);
+  }
+  checkTrialer();
+  getMovieID();
+}
+function filterAges() {
+  html = '';
+  for (j = 0; j < ageLimit.length; j++) {
+    for (i = 0; i < jsonMovies.length; i++) {
+      if (jsonMovies[i].rated == ageLimit[j]) {
+        readHTML();
+        $('#screening-result').html(html);
+      }
     }
   }
+  ageLimit = [];
+  checkTrialer();
   getMovieID();
 }
 
-function getMovieID() {
-  $('.movieAll').on('click', function () {
-    //Get the id of the clicked html tag
-    movieID = $(this).attr('id');
-    //Store the movie id to localStorage so that when we change the webpage 
-    //we could still get the movie id from localStorage
-    localStorage.setItem('ID', movieID);
-  });
+function filterMovies() {
+  html = '';
+  for (i = 0; i < jsonMovies.length; i++) {
+    if (movie == jsonMovies[i].id) {
+      readHTML();
+      $('#screening-result').html(html);
+    }
+  }
+  ageLimit = [];
+  checkTrialer();
+  getMovieID();
+}
+
+function ageAndMovies() {
+  html = '';
+  for (j = 0; j < ageLimit.length; j++) {
+    for (i = 0; i < jsonMovies.length; i++) {
+      if (jsonMovies[i].rated == ageLimit[j] && movie == jsonMovies[i].id) {
+        readHTML();
+      }
+      $('#screening-result').html(html);
+    }
+    if (html == '') {
+      html = `<p class="name">No Movie Match....</p>`;
+      $('#screening-result').html(html);
+      html = '';
+    }
+  }
+  ageLimit = [];
+  getMovieID();
+  checkTrialer();
 }
 readInfoJson();
 
