7c92b42cb094de46836eeb286043471af962b5fa
herrgallardo <gallardogirela@gmail.com>
2021-12-20 09:38:39 +0100

    minor code changes

--- frontend/js/mybookings.js
+++ frontend/js/mybookings.js
@@ -24,15 +24,15 @@ function renderTicketInfo() {
     document.getElementById("ticketsinfo").innerHTML = ticketData;
 }
 readTicketJson();
-async function readTicket(){
+async function readTicket() {
     let theTicket = await fetch('json/ticket.json');
     theTicketfound = await theTicket.json();
     findMyTicket(theTicketfound);
 }
-function findMyTicket(){
+function findMyTicket() {
     for (let i = 0; i < myTickets.length; i++) {
         if (ticket === myTickets[i].ticketNumber) {
-            return  `
+            return `
             <p>MY TICKET: </p>
     <p>Movie: ${myTickets[i].movieName}</p>
     <p>Date: ${myTickets[i].date}</p>
@@ -40,5 +40,7 @@ function findMyTicket(){
     <p>Seat: ${myTickets[i].seat}</p>
     <p>Ticket Number: ${myTickets[i].ticketNumber}</p>
     `;
-    document.getElementById("searchTicket").innerHTML = ticketData;
+            document.getElementById("searchTicket").innerHTML = ticketData;
+        }
+    }
 }
